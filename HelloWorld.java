f68395b732c224236f3d63b264f45604cfbf83e0
l00162530 <l00162530@atu.ie>
2022-10-06 03:35:10 +0100

    Modify HelloWorld.Java - correct the class name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,6 @@
-public class HelloLYIT {
+//the name of the class HelloLYIT does not match the file name
+//this will raise error in java hence must be fixed
+public class HelloWorld {
     public static void main(String[] args) {
     // Prints "Hello, LYIT from Ruth" as output
     System.out.println("Hello, LYIT from Ruth");
